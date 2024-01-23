000d0e26115a374899f53c756d3c5c9d0c74801e
Thong Kuah <tkuah@gitlab.com>
2024-01-23 16:22:57 +1300

    Allow definitions within a task that is within a namespace.
    This is similar to definitions within a task (without a namespace).

--- lib/rubocop/cop/rake/helper/task_definition.rb
+++ lib/rubocop/cop/rake/helper/task_definition.rb
@@ -16,9 +16,23 @@ module RuboCop
             )
           PATTERN
 
+          def_node_matcher :task?, <<-PATTERN
+            (block
+              (send _ {:task} ...)
+              args
+              _
+            )
+          PATTERN
+
           def in_namespace?(node)
             node.each_ancestor(:block).any? do |a|
               namespace?(a)
+            end
+          end
+
+          def in_task?(node)
+            node.each_ancestor(:block).any? do |a|
+              task?(a)
             end
           end
         end
